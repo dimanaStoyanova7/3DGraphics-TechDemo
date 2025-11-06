b91567618b4764e16c91b2eb54610bc29e97d8bf
Luka Roginic <87370880+snike15243@users.noreply.github.com>
2025-11-06 15:13:09 +0100

    Revert "Tile generation + day/night system"

--- shaders/shader_vert.glsl
+++ shaders/shader_vert.glsl
@@ -1,26 +1,24 @@
-// --- CORRECTED shaders/shader_vert.glsl ---
-#version 410 core
+#version 410
 
 uniform mat4 mvpMatrix;
 uniform mat4 modelMatrix;
-
+// Normals should be transformed differently than positions:
+// https://paroj.github.io/gltut/Illumination/Tut09%20Normal%20Transformation.html
 uniform mat3 normalModelMatrix;
 
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 texCoord;
 
-// ONLY Per-Vertex data should be passed as 'out'
-out vec3 gPosition;
-out vec3 gNormal;
-out vec2 gTexCoord;
+out vec3 fragPosition;
+out vec3 fragNormal;
+out vec2 fragTexCoord;
 
 void main()
 {
     gl_Position = mvpMatrix * vec4(position, 1);
     
-    gPosition   = (modelMatrix * vec4(position, 1)).xyz;
-    gNormal     = normalModelMatrix * normal;
-    gTexCoord   = texCoord;
-
+    fragPosition    = (modelMatrix * vec4(position, 1)).xyz;
+    fragNormal      = normalModelMatrix * normal;
+    fragTexCoord    = texCoord;
 }
